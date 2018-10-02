fe1320103374e3cde1c4deadf6538b8497f162ee
debratton <debratton@gmail.com>
2018-10-01 21:31:27 -0500

    Code Added

---
+++ GoodEatins/GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  GoodEatins
+//
+//  Created by David E Bratton on 10/1/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
+
+
